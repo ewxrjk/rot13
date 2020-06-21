83baaa06a63146beef9559f518f8442c4258688f
Richard Kettlewell <rjk@greenend.org.uk>
2020-06-21 19:02:53 +0100

    WIP

--- rot13.h
+++ rot13.h
@@ -4,6 +4,15 @@
 
 #include <sys/types.h>
 
+#ifndef VECTORIZE
+#if __GNUC__
+#define VECTORIZE 1
+#else
+#define VECTORIZE 0
+#endif
+#endif
+
+#if VECTORIZE
 /* Overkill but fun */
 #if __AVX__
 typedef unsigned char vector __attribute__((vector_size(32)));
@@ -36,11 +45,32 @@ typedef unsigned char vector __attribute__((vector_size(16)));
     buffer += sizeof(vector);                                                  \
     n -= sizeof(vector);                                                       \
   } while(0)
+#else
+#define STEP()                                                                 \
+  do {                                                                         \
+    int v = *buffer;                                                           \
+    const int bupl = (v >= 'A');                                               \
+    const int buph = (v <= 'M');                                               \
+    const int blpl = (v >= 'a');                                               \
+    const int blph = (v <= 'm');                                               \
+    const int bp = (bupl & buph) | (blpl & blph);                              \
+    const int bunl = (v >= 'N');                                               \
+    const int bunh = (v <= 'Z');                                               \
+    const int blnl = (v >= 'n');                                               \
+    const int blnh = (v <= 'z');                                               \
+    const int bn = (bunl & bunh) | (blnl & blnh);                              \
+    *buffer = v + (-bp & 13) - (-bn & 13);                                     \
+    buffer++;                                                                  \
+    n--;                                                                       \
+  } while(0)
+#endif
 
 static void rot13(char *buffer, ssize_t n) {
+#if VECTORIZE
   const vector v_A = V('A'), v_M = V('M'), v_N = V('N'), v_Z = V('Z');
   const vector v_a = V('a'), v_m = V('m'), v_n = V('n'), v_z = V('z');
   const vector v_13 = V(13);
+#endif
   while(n > 0) {
     STEP();
     STEP();
